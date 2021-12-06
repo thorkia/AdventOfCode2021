8c275a3aa85afbed971b41490cd6be31677be779
Robert Maissan <thorkia@gmail.com>
2021-12-06 11:06:58 -0500

    Day 6 challenge solution

---
+++ Day06/main.py
@@ -0,0 +1,48 @@
+import time
+
+filename = '.\\Day06\\input.txt'
+#filename = '.\\Day06\\testinput.txt'
+
+with open(filename) as f:
+    items = [int(item) for item in f.readline().strip().split(",")]    
+    f.close()
+
+dayDict = dict.fromkeys(range(0,9),0)
+
+for item in items:
+    dayDict[item] += 1
+
+print(dayDict)
+
+startns = time.time_ns()
+
+#Part 1 -> count 80 days
+for day in range(1,81):
+    day0 = dayDict[0]
+    for i in range(1,9):
+        dayDict[i-1] = dayDict[i]
+    
+    #add day 0 to day 6 and set day day 8 to day 6
+    dayDict[6] += day0
+    dayDict[8] = day0
+    #print(dayDict)
+
+print(str(sum(dayDict.values())))
+
+#part 2 -> 256 day
+for day in range(81,257):
+    day0 = dayDict[0]
+    for i in range(1,9):
+        dayDict[i-1] = dayDict[i]
+    
+    #add day 0 to day 6 and set day day 8 to day 6
+    dayDict[6] += day0
+    dayDict[8] = day0
+    #print(dayDict)
+
+print(str(sum(dayDict.values())))
+
+endns = time.time_ns()
+
+elapsedTime = (endns - startns) / 1000000
+print(elapsedTime)
